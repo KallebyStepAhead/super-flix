0892d509a8a6e19246d9a47e4e0172ffd67e1845
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2021-12-31 15:52:26 +0000

    📦 NEW: Creating NextJs document file

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <title>SuperFLIX</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
