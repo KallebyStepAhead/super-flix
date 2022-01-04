ca9d2d0cdcf00054920d3975c8eec83a590b6759
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-01-04 13:48:34 +0000

    📦 NEW: Get latest video function

---
+++ modules/home/functions/getLastVideo.ts
@@ -0,0 +1,45 @@
+import { gql, TypedDocumentNode } from '@apollo/client';
+import client from '../../../shared/services/apollo';
+import { Video } from '../schemas/video';
+
+type GetLastVideoData = {
+  videos: Video[]
+}
+
+type GetLastVideoResult = {
+  lastVideo: Video
+};
+
+export const getVideosQuery: TypedDocumentNode<GetLastVideoData> = gql`
+  query {
+    videos(last: 1) {
+      id
+      title
+      description
+      thumbnail {
+        url
+      }
+      videoContent {
+        url
+      }
+      primaryBackground {
+        url
+      }
+      secondaryBackground {
+        url
+      }
+    }
+  }
+`;
+
+export async function getLastVideo(): Promise<GetLastVideoResult> {
+  const { data: { videos } } = await client.query({
+    query: {
+      ...getVideosQuery,
+    },
+  });
+
+  return {
+    lastVideo: videos[0] || undefined,
+  };
+}
