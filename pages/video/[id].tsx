a6caf453f8fbc4fdd849840efaf72ce3867c10b3
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-01-05 12:42:30 +0000

    📦 NEW: Creating Dynamic static video page to display videos by id

---
+++ pages/video/[id].tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
+import { Container } from '@chakra-ui/react';
+import { ParsedUrlQuery } from 'querystring';
+import { Video } from '../../modules/video/schemas/video';
+import { getVideos } from '../../modules/video/functions/getVideos';
+import { getVideoById } from '../../modules/video/functions/getVideoById';
+
+type IVideoParams = ParsedUrlQuery & {
+  id: string
+}
+
+type VideoPageProps = {
+  data: Video
+}
+
+type VideoPageStaticProps = {
+  data: Video | null
+}
+
+const VideoPage: NextPage<VideoPageProps> = ({ data }) => (
+  <Container
+    maxW="full"
+  >
+    {JSON.stringify(data)}
+  </Container>
+);
+
+export default VideoPage;
+
+export const getStaticPaths: GetStaticPaths<IVideoParams> = async () => {
+  const { videos } = await getVideos();
+
+  const paths = videos.map((video) => ({
+    params: {
+      id: video.id,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+type VideoPageGetStaticProps = GetStaticProps<VideoPageStaticProps, IVideoParams>
+export const getStaticProps: VideoPageGetStaticProps = async (context) => {
+  const videoId = context.params?.id;
+
+  const { video } = await getVideoById(videoId || '');
+
+  if (!video) {
+    return {
+      redirect: {
+        destination: '/',
+      },
+      props: {
+        data: null,
+      },
+    };
+  }
+
+  return {
+    props: {
+      data: video,
+    },
+    revalidate: 3600 * 8,
+  };
+};
