cfc41101a5e94cb07510e7dc6aa145e59faf763c
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-01-05 12:41:50 +0000

    📦 NEW: Creating get videos function, for video module

---
+++ modules/video/functions/getVideos.tsx
@@ -0,0 +1,40 @@
+import { gql, TypedDocumentNode } from '@apollo/client';
+import client from '../../../shared/services/apollo';
+import { Video } from '../schemas/video';
+
+type GetVideosData = {
+  videos: Video[]
+}
+
+type GetVideosResult = GetVideosData;
+
+export const getVideosQuery: TypedDocumentNode<GetVideosData> = gql`
+  query {
+    videos (last: 10) {
+      id
+      title
+      description
+      thumbnail {
+        url
+      }
+      videoContent {
+        url
+      }
+      secondaryBackground {
+        url
+      }
+    }
+  }
+`;
+
+export async function getVideos(): Promise<GetVideosResult> {
+  const { data: { videos } } = await client.query({
+    query: {
+      ...getVideosQuery,
+    },
+  });
+
+  return {
+    videos,
+  };
+}
