d89f38fb2ab141357f30d28011e0e272b2a7e23d
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-01-25 22:04:21 +0000

    📦 NEW: Creating next auth file with credentials auth flow

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,48 @@
+import NextAuth from 'next-auth';
+import { compare } from 'bcrypt';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { getUserByEmail } from '../../../modules/auth/functions/getUserByEmail';
+
+export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: '/auth/signIn',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Email and Password',
+      credentials: {
+        email: {
+          type: 'email',
+        },
+        password: {
+          type: 'password',
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          throw new Error('Wrong credentials or User not exists. Try again.');
+        }
+
+        const { user } = await getUserByEmail(credentials.email);
+
+        if (!user) {
+          throw new Error('Wrong credentials or User not exists. Try again.');
+        }
+
+        const isValid = await compare(credentials.password, user.password);
+
+        if (!isValid) {
+          throw new Error('Wrong credentials or User not exists. Try again.');
+        }
+
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          avatarUrl: user.avatarUrl,
+        };
+      },
+    }),
+  ],
+});
