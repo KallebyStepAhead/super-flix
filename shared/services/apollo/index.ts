5a19899ed97f8f94763cebf4926b02945fea7c00
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-01-25 21:54:25 +0000

    🐛 FIX: Setting up Apollo client to handle errors

--- shared/services/apollo/index.ts
+++ shared/services/apollo/index.ts
@@ -1,11 +1,22 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+type ErrorResponse = {
+  message: string
+}
+
+export type NetworkGenericErrorResponse = {
+  result: {
+    errors: ErrorResponse[]
+  }
+}
+
 const client = new ApolloClient({
   uri: process.env.API_URI,
   cache: new InMemoryCache(),
   headers: {
     authorization: `Bearer ${process.env.API_TOKEN}`,
   },
+  ssrMode: true,
 });
 
 export default client;
