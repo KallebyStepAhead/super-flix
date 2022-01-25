d61b8865763b6621cb7c36fd0e739f3ab02c38c5
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-01-25 22:07:30 +0000

    📦 NEW: Adding Session Provider do MyApp component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider } from '../shared/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SessionProvider session={pageProps.session}>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
