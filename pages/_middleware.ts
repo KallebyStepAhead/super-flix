a57a281038fba33db4012c54bd0bbfa6c19b007a
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-02-05 17:41:13 +0000

    Test Middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,41 @@
+import { NextApiRequest } from 'next';
+import { getToken } from 'next-auth/jwt';
 import { NextMiddleware, NextResponse } from 'next/server';
 
-export const middleware: NextMiddleware = () => {
+export const middleware: NextMiddleware = async (req) => {
   console.log('Running middleware');
 
-  return NextResponse.next();
+  const { pathname } = req.nextUrl;
+
+  const secret = process.env.NEXTAUTH_SECRET;
+
+  if (!secret) {
+    console.log('Secret Error');
+    return NextResponse.error();
+  }
+
+  const token = await getToken({
+    req: req as unknown as NextApiRequest,
+    secret,
+  });
+
+  console.log('auth token', token);
+
+  const isAuthRoute = (
+    pathname.includes('/api/auth')
+    || pathname === '/auth/signIn'
+    || pathname === '/auth/signUp'
+  );
+
+  const isAllowed = !!token || isAuthRoute;
+
+  if (isAllowed) {
+    console.log('allowed');
+    return NextResponse.next();
+  }
+
+  console.log('redirect to signIn');
+
+  // If user neither authenticated nor is auth route, redirect.
+  return NextResponse.redirect('/api/auth/signin');
 };
