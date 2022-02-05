6984d5a617e1ab34793f3b9b0d5cbb372b2a26a7
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-02-05 18:15:53 +0000

    Test Middleware

---
+++ pages/_middleware.js
@@ -0,0 +1,32 @@
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function middleware(req) {
+  const { pathname } = req.nextUrl;
+
+  const secret = process.env.NEXTAUTH_SECRET;
+
+  if (!secret) {
+    return NextResponse.error();
+  }
+
+  const token = await getToken({
+    req,
+    secret,
+  });
+
+  const isAuthRoute = (
+    pathname.includes('/api/auth')
+    || pathname === '/auth/signIn'
+    || pathname === '/auth/signUp'
+  );
+
+  const isAllowed = !!token || isAuthRoute;
+
+  if (isAllowed) {
+    return NextResponse.next();
+  }
+
+  // If user neither authenticated nor is auth route, redirect.
+  return NextResponse.redirect('/api/auth/signin');
+}
--- pages/auth/_middleware.js
+++ pages/auth/_middleware.js
@@ -1,29 +1,21 @@
 /* eslint-disable @next/next/no-server-import-in-page */
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
-import type { NextApiRequest } from 'next';
-import type { NextRequest } from 'next/server';
 
-export async function middleware(req: NextRequest) {
-  console.log('Running Auth middleware');
-
+export async function middleware(req) {
   const secret = process.env.NEXTAUTH_SECRET;
 
   if (!secret) {
-    console.log('Secret Error');
     return NextResponse.error();
   }
 
   const token = await getToken({
-    req: req as any as NextApiRequest,
+    req,
     secret,
   });
 
-  console.log('Auth-> AuthToken:', token);
-
   // If user is already authenticated, redirect to index route
   if (token) {
-    console.log('Redirect to Index');
     return NextResponse.redirect('/');
   }
 
