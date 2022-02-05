93310cdec6d450d6ab09563db12eaaebef06a0c0
Kalleby Santos <kalleby.santos@stepaheadcons.onmicrosoft.com>
2022-02-05 17:35:32 +0000

    🐛 FIX: Test Middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,3 +1,7 @@
 import { NextMiddleware, NextResponse } from 'next/server';
 
-export const middleware: NextMiddleware = () => NextResponse.next();
+export const middleware: NextMiddleware = () => {
+  console.log('Running middleware');
+
+  return NextResponse.next();
+};
